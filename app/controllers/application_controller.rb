b4ba99ab641c728447f697370032757021eb0739
Guenole Quideau <gqprof@protonmail.com>
2021-11-15 17:37:56 +0100

    pb de token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def new
+    # TODO: generate random grid of letters
+    grid_size = 10
+    grid = []
+    grid_size.times { grid << ("A".."Z").to_a.sample }
+    @letters = grid
+  end
+
+  def score
+    raise
+  end
 end
