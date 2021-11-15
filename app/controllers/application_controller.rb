511acddb4c6af916baa41b85a62c434de5a69ccf
Guenole Quideau <gqprof@protonmail.com>
2021-11-16 00:36:36 +0100

    Longest word OK without score

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def new
-    # TODO: generate random grid of letters
-    grid_size = 10
-    grid = []
-    grid_size.times { grid << ("A".."Z").to_a.sample }
-    @letters = grid
-  end
-
-  def score
-    raise
-  end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,37 @@
+require 'open-uri'
+# require 'json'
+# require 'date'
+
 class GamesController < ApplicationController
+  def new
+    # TODO: generate random grid of letters
+    grid = []
+    10.times { grid << ('A'..'Z').to_a.sample }
+    @letters = grid
+  end
+
+  def score
+    @letters = params[:letters].split
+    @attempt = params[:word].upcase
+    @english = english?(@attempt)
+    @valide = valide?(@attempt, @letters)
+  end
+
+  private
+
+  # Tester l'attempt avec l'API Wagon Dictionary et récupérer fichier JSON
+  # {"found":false,"word":"zzzz","error":"word not found"}
+  # {"found":true,"word":"apple","length":5}
+  def english?(attempt)
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    json = JSON.parse(URI.open(url).read)
+    json['found']
+  end
+
+  # Validite de l'attempt avec la grille :
+  #   - mettre mot sous forme d'array
+  #   - valide si tous les caract du mot sont en nb. =< a ceux de la grille
+  def valide?(attempt, grid)
+    attempt.chars.all? { |char| attempt.count(char) <= grid.count(char) }
+  end
 end
